ae34dbd8d2f10c55cc52da65052ee0003197f2cc
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-22 11:51:22 -0300

    Inicial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Header extends Component{
+  render(){
+    return (
+    <div id="Header">
+      <div><h2>facebook</h2></div>
+      <div><h4>Meu Perfil</h4></div>
+    </div>
+    )
+  }
+}
+
+export default Header;
